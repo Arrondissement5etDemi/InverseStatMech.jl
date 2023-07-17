bd7281b6817c793cfe58ae11ead43c19a954c90d
Arrondissement5etDemi <hainaw@princeton.edu>
2023-07-16 20:13:03 -0400

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,14 @@
 using Spec2Struc, SpecialFunctions
 using Test
 
-@testset "Spec2Struc.jl" begin
+@testset "reverse_monte_carlo" begin
+    kF = 2*√π
+    targ_g2(r) = 1 - 4*besselj(1, kF*r)^2/(kF*r)^2
+    @test Spec2Struc.reverse_mc(2, 500, 1, targ_g2; test = true)
+end
+
+
+@testset "torquato-wang algorithm" begin
     kF = 2*√π
     function targ_s(k)
         if k ≤ 2kF
@@ -13,5 +20,6 @@ using Test
     targ_g2(r) = 1 - 4*besselj(1, kF*r)^2/(kF*r)^2
     pot(r, params) = -params[1]*exp(-(r/params[2])^2)*log(r) + √π/(2*r)*(1 - exp(-(r/params[3])^2))
     params_vec = [2.0, 1.0, 1.0] #[2.099144746573296, 0.666655413783426, 0.4739706702894683]
-    @test Spec2Struc.optim_parametrized_pot(params_vec, pot, 2, 1.0, targ_g2, targ_s; test = true)[1]
+    @test Spec2Struc.optim_parametrized_pot(params_vec, pot, 2, 1.0, targ_g2, targ_s; Ψ_tol = 0.01, test = true)
 end
+
