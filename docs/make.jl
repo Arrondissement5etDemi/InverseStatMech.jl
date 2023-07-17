d563b768964084613d3108ade885c0ebf7049bdf
Arrondissement5etDemi <hainaw@princeton.edu>
2023-07-17 02:15:56 -0400

    change package name

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,14 @@
 import Pkg; Pkg.add("Documenter")
 push!(LOAD_PATH,"../src/")
-using Spec2Struc
+using InvStatMech
 using Documenter
 
 makedocs(
-         sitename = "Spec2Struc.jl",
-         modules  = [Spec2Struc],
+         sitename = "InvStatMech.jl",
+         modules  = [InvStatMech],
          pages=[
                 "Home" => "index.md"
                ])
 deploydocs(;
-    repo="github.com/Arrondissement5etDemi/Spec2Struc.jl",
+    repo="github.com/Arrondissement5etDemi/InvStatMech.jl",
    )
